a51f2ab0d24591a79024d585a7fa949fda706ef8
eritho91 <erik.thorell@iths.se>
2026-01-27 10:37:10 +0100

    Initial commit

---
+++ src/main/java/se/iths/erikthorell/javaverktyg_labb/JavaverktygLabbApplication.java
@@ -0,0 +1,13 @@
+package se.iths.erikthorell.javaverktyg_labb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaverktygLabbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaverktygLabbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/erikthorell/javaverktyg_labb/JavaverktygLabbApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.erikthorell.javaverktyg_labb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaverktygLabbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
